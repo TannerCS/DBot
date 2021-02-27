86b7d6396de25653138941bab41dc15087bd0739
Tanner <tannersmithasdf@hotmail.com>
2021-02-27 14:47:43 -0600

    Add delete message command

---
+++ Commands/Moderation/ManageMessages.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBot.Commands.Moderation
+{
+    public class ManageMessages : ModuleBase<SocketCommandContext>
+    {
+        [Command("delete"),
+         Summary("Deletes the specified message.")]
+        [RequireBotPermission(Discord.ChannelPermission.ManageMessages)]
+        public async Task DeleteMessageAsync([Optional]ulong messageID)
+        {
+            var referencedMessage = Context.Message.ReferencedMessage ?? await Context.Channel.GetMessageAsync(messageID);
+            if (referencedMessage == null)
+            {
+                await ReplyAsync("Could not find message.");
+                return;
+            }
+
+            await referencedMessage.DeleteAsync();
+        }
+    }
+}
