31c5edeeb40e32185db06d339d7107a6d7aa84e7
Tanner <tannersmithasdf@hotmail.com>
2021-02-27 14:55:07 -0600

    Error check delete message command

--- Commands/Moderation/ManageMessages.cs
+++ Commands/Moderation/ManageMessages.cs
@@ -12,9 +12,10 @@ namespace DBot.Commands.Moderation
         [Command("delete"),
          Summary("Deletes the specified message.")]
         [RequireBotPermission(Discord.ChannelPermission.ManageMessages)]
+        [RequireUserPermission(Discord.ChannelPermission.ManageMessages)]
         public async Task DeleteMessageAsync([Optional]ulong messageID)
         {
-            var referencedMessage = Context.Message.ReferencedMessage ?? await Context.Channel.GetMessageAsync(messageID);
+            var referencedMessage = Context.Message.ReferencedMessage ?? (messageID != 0 ? await Context.Channel.GetMessageAsync(messageID) : null);
             if (referencedMessage == null)
             {
                 await ReplyAsync("Could not find message.");
