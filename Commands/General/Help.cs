d1d12c9bc20add0b43818e9536ef94b10a858e2d
Tanner <tannersmithasdf@hotmail.com>
2021-02-28 14:44:57 -0600

    Add help command

---
+++ Commands/General/Help.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBot.Commands.General
+{
+    public class Help : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        [Summary("Displays help info about commands. Usage: <prefix>help")]
+        public async Task HelpCommand()
+        {
+            var commands = DiscordBot.CommandService.Commands;
+            var embed = new EmbedBuilder();
+
+            embed.WithTitle("Commands");
+            foreach (var command in commands)
+                embed.AddField(command.Name, command.Summary, true);
+
+            await ReplyAsync(embed: embed.Build());
+        }
+    }
+}
